cf31c29011c08e2392bac04d0da81eee6062729b
Nate <nate.tripp13448@gmail.com>
2019-06-19 15:45:44 -0600

    Finsished site

--- tnt.lsdwebsite/Controllers/HomeController.cs
+++ tnt.lsdwebsite/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using tnt.lsdwebsite.Models;
 
 namespace tnt.lsdwebsite.Controllers
 {
@@ -13,17 +15,23 @@ namespace tnt.lsdwebsite.Controllers
             return View();
         }
 
-        public ActionResult About()
+
+        [HttpPost]
+        public ActionResult Index(ContactFormModel form)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            if (ModelState.IsValid)
+            {
+                SmtpClient client = new SmtpClient();
+                MailMessage mail = new MailMessage();
+                mail.To.Add("nate.tripp13448@gmail.com");
+                mail.Subject = "LSD";
+                mail.Body = form.name + "\n" +
+                    form.businessName + "\n" +
+                    form.email + "\n" +
+                    form.phoneNumber + "\n\n" +
+                    form.message;
+                client.Send(mail);
+            }
             return View();
         }
     }
---
+++ tnt.lsdwebsite/Models/ContactFormModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace tnt.lsdwebsite.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        [DisplayName("Name")]
+        public string name { get; set; }
+
+        [DisplayName("Business name")]
+        public string businessName { get; set; }
+
+        [Required]
+        [Phone]
+        [DisplayName("Phone number")]
+        public string phoneNumber{ get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email")]
+        public string email { get; set; }
+
+        [DisplayName("Questions and inquiries")]
+        [DataType(DataType.MultilineText)]
+        public string message{ get; set; }
+    }
+}
